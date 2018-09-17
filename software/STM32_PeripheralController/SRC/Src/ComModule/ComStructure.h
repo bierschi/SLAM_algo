9131f94a974aa49178f44caec7ca1bfcb24831ad
stgrey9842 <42148365+stgrey9842@users.noreply.github.com>
2018-09-17 15:49:23 +0200

    extended ComStructure.h file, added members for odometry, distance sensors, etc.

--- software/STM32_PeripheralController/SRC/Src/ComModule/ComStructure.h
+++ software/STM32_PeripheralController/SRC/Src/ComModule/ComStructure.h
@@ -1,6 +1,11 @@
 /*
  * ComStructure.h
  *
+ * Defines the global communication struct transmitted over SPI (DMA)
+ *
+ * The Raspberry Pi also needs the copy of this header, thus the sent data has to be in sync
+ * perfectly.
+ *
  *  Created on: 09.09.2018
  *      Author: stefan
  */
@@ -8,10 +13,11 @@
 #ifndef COMSTRUCTURE_H
 #define COMSTRUCTURE_H
 
-
+// define some available steering modes on STM32 board
 #define COM_STEERING_MODE_AUTO 1u
 #define COM_STEERING_MODE_MANUAL 2u
 
+// define some initialization values for the communication structure
 #define COM_STEERING_SPEED_ZERO 0u
 #define COM_STEERING_DIRECTON_ZERO 0u
 #define COM_STEERING_ANGLE_ZERO 0.0f
@@ -20,13 +26,30 @@
 #pragma pack(push, 1)
 
 typedef struct {
-	uint8_t CurrentSteeringMode; // AUTO, MANUAL
-	uint16_t CurrentSteeringSpeed; // Desired Steering Speed in 0...1000
-	uint8_t CurrentSteeringDirection; // forward or backward
-	float CurrentSteeringAngle; // Desired Steering Angle in Degree
+	// select steering mode AUTO or MANUAL
+	uint8_t CurrentSteeringMode;
 
+	// Desired Steering Speed in 0...1000
+	uint16_t CurrentSteeringSpeed;
+
+	// motor forward or backward (0 ... 1)
+	uint8_t CurrentSteeringDirection;
+
+	// Desired Steering Angle in Degree (rel. for manual mode, unit: degree)
+	float CurrentSteeringAngle;
+
+	// target point x (rel. for automatic steering, unit: mm)
 	float Target_X;
+	// target point y (rel. for automatic steering, unit:mm)
 	float Target_Y;
+
+	// current orientation angle in degree
+	float CurrentOrientation;
+
+	// ultrasonic distance sensor values in front (unit: mm)
+	uint16_t USDistanceFront;
+	// ultrasonic distance sensor values in rear (unit: mm)
+	uint16_t USDistanceRear;
 } ComStructureType;
 
 #pragma pack(pop)
