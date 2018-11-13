82f4ffec18cf0d6572edd793db900b11b6dc2b16
Christian Bierschneider <christian.bierschneider@web.de>
2018-11-13 19:14:37 +0100

    Typo

--- software/final/src/slam/SlamMap.cpp
+++ software/final/src/slam/SlamMap.cpp
@@ -266,7 +266,7 @@ void SlamMap::createTxtPositionFile() {
 
     std::string mapCounterStr = std::to_string(mapCounter);
     //std::string positionDataFile = "position" + mapCounterStr +  ".txt";
-    std::string positionDataFile = "position" + ".txt"
+    std::string positionDataFile = "position.txt";
     FILE* out = fopen(positionDataFile.c_str(), "w");
     fprintf(out, "%d\n%d\n%.2f", getPixelX(), getPixelY(), theta);
     fclose(out);
