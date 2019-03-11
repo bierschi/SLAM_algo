1c973a7ccf400e8b36679043d93a99aa7d1a2e7b
stgrey9842 <42148365+stgrey9842@users.noreply.github.com>
2019-03-11 16:56:03 +0100

    prepared merge of alf_main.cpp

--- software/final/alf_car/alf_main.cpp
+++ software/final/alf_car/alf_main.cpp
@@ -1,17 +1,34 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 
 #include "ros/ros.h"
 #include "SlamMap.h"
 #include "Server.h"
 #include "PathfinderInterface.h"
+#include "StateModel.h"
+
+// signalhandler for termination of program
+void sigIntHandler(int signum);
+bool exitProgram = false;
+
+// #########################################################################################
+// signal handlers
+// #########################################################################################
+
+void sigIntHandler(int signum)
+{
+	exitProgram = true;
+}
 
 
 int main(int argc, char** argv) {
 
 
     std::cout << "starting alf software" << std::endl;
+
+    signal(SIGINT, sigIntHandler);
 
     ros::init(argc, argv, "Alf");
 
@@ -21,15 +38,29 @@ int main(int argc, char** argv) {
 
     PathfinderInterface* pi = new PathfinderInterface(*sm);
 
+    //################################################
+    // Create StateModel to control vehicle (SPIInterface to STM32)
+    StateModel spiInterfaceStateModel;
+
+    spiInterfaceStateModel.setScanAtStartup(true);
+
+    printf("Open SPI Interface...\n");
+	spiOpen();
+
+	printf("Create State Model...\n");
+    spiInterfaceStateModel.Init();
+    //################################################
+
     bool flag = true;
 
-    while ( ros::ok() ) {
+    while ( ros::ok() && exitProgram == false) {
 
         if (sm->getMapInitFlag() & sm->getPoseInitFlag()) {
 
             if (flag) {
                 std::cout <<"Map and Pose was succesfully initialised!" << std::endl;
                 pi->processPath();
+                spiInterfaceStateModel.updatePathTravels(test);
                 sm->setSaveMap(true);
                 flag = false;
             }
@@ -37,11 +68,19 @@ int main(int argc, char** argv) {
             std::cout << "x_pixel: " << sm->getPixelX() << " y_pixel: " << sm->getPixelY() << " theta: " << sm->theta << std::endl;
         }
 
+        spiInterfaceStateModel.updatePosition(sm->getPixelX(),sm->getPixelY(), sm->theta);
+        spiInterfaceStateModel.Main();
+
         //sm->createTxtPositionFile();
         //usleep(200000);
         ros::spinOnce();
     }
 
+    printf("Close Model...\n");
+    model.Close();
+
+    printf("Close SPI Interface...\n");
+    spiClose();
 
     return 0;
 }
